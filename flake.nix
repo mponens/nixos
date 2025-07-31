e5dd235681bb9b5f25dd7de2f001af2f43876643
Daniel Jin <mrpickles123115@gmail.com>
2025-07-31 10:47:04 -0600

    done

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A simple NixOS flake";
 
-  inputs = {
+  inputs = { 
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
