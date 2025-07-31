e2e4666fb073246c89562481b6b992d66b8f8ad3
Daniel Jin <mrpickles123115@gmail.com>
2025-07-31 16:23:07 -0600

    test lib thing

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, myLib, ... }: {
   home.file.".config/nvim" = {
     source = config.lib.file.mkOutOfStoreSymlink ./nvim;
     recursive = true;
--- lib/filesystem.nix
+++ lib/filesystem.nix
@@ -1,5 +1,4 @@
-{ config, username } : {
+{ username } : {
   storagePath = "/home/${username}/.config/nixos";
   homeManagerPath = "${storagePath}/home";
-  mkOOSSHome = path : (config.lib.file.mkOutOfStoreSymlink "${homeManagerPath}/${path}");
 }
