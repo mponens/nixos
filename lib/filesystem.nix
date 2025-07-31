7abfd683d81e0cfc673fbc9af1ea723d669b41c7
Daniel Jin <mrpickles123115@gmail.com>
2025-07-31 16:25:15 -0600

    readd mkOOSS

--- lib/filesystem.nix
+++ lib/filesystem.nix
@@ -1,4 +1,5 @@
-{ username } : {
+{ config, username } : {
   storagePath = "/home/${username}/.config/nixos";
   homeManagerPath = "${storagePath}/home";
+  mkOOSSToHome = path : (config.lib.file.mkOutOfStoreSymlink "${homeManagerPath}/${path}");
 }
