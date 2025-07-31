20161ac87b36119adfe6602974f669732396f21e
Daniel Jin <mrpickles123115@gmail.com>
2025-07-31 11:30:15 -0600

    core

--- users/daniel/home.nix
+++ users/daniel/home.nix
@@ -1,9 +1,7 @@
 { username, ... }: {
-  home.username = "${username}";
-  home.homeDirectory = "/home/${username}";
-  home.stateVersion = "25.05";
 
   imports = [
+    ../../home/core.nix
     ../../home/programs
   ];
 
