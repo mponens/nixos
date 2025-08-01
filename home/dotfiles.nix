6ba893341b64ef3cdfab573c568c8b5c8327cb99
Daniel Jin <mrpickles123115@gmail.com>
2025-08-01 10:03:56 -0600

    immut

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,6 +1,7 @@
 { pkgs, config, username, ... }: 
 let 
   dotfiles = config.lib.file.mkOutOfStoreSymlink /home/${username}/.config/nixos/home/dotfiles;
+  immutableDotfiles = ./dotfiles;
 in {
   home.file.".config/nvim" = {
 	source = "${dotfiles}/nvim";
