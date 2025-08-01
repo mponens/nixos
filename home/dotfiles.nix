c017f60a1b8086eadd3a8df47a20c2827459a320
Daniel Jin <mrpickles123115@gmail.com>
2025-08-01 10:21:07 -0600

    tmux conf

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,11 +1,22 @@
 { pkgs, config, username, ... }: 
 let 
   dotfiles = config.lib.file.mkOutOfStoreSymlink /home/${username}/.config/nixos/home/dotfiles;
-  immutableDotfiles = ./dotfiles;
+  iDotfiles = ./dotfiles; # Immutable config
 in {
   home.file.".config/nvim" = {
 	source = "${dotfiles}/nvim";
     recursive = true;
   };
   home.file.".vimrc".source = "${dotfiles}/nvim/.vimrc";
+
+  home.file.".tmux.conf".source = "${iDotfiles}/tmux/tmux.conf";
+  home.file.".tmux/plugins/tpm" = {
+    source = pkgs.fetchFromGitHub {
+	  owner = "tmux-plugins";
+	  repo = "tpm";
+	  rev = "99469c4a9b1ccf77fade25842dc7bafbc8ce9946";
+      hash = "sha256-hW8mfwB8F9ZkTQ72WQp/1fy8KL1IIYMZBtZYIwZdMQc=";
+	};
+	recursive = true;
+  };
 }
