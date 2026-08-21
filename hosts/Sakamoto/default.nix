83ac304e911c3fd63790dec65eb73258f8c276b7
Daniel Jin <mrpickles123115@gmail.com>
2026-08-20 20:01:08 -0400

    attempt fixing amd suspend sometimes killing laptop instead

--- hosts/Sakamoto/default.nix
+++ hosts/Sakamoto/default.nix
@@ -3,13 +3,17 @@
 {
   imports = [
       ../../modules/audio.nix
-	  ../../modules/av_intel.nix
+	  ../../modules/av_amd.nix
       ../../modules/bluetooth.nix
       ../../modules/desktop.nix
       ../../modules/fonts.nix
       ../../modules/laptop.nix
       ../../modules/system.nix
       ../../modules/sops.nix
+
+	  ../../modules/fwupd.nix
+
+
       ../../modules/extraHosts.nix	
 	  ../../modules/extra/openocd.nix
 	  ../../modules/extra/udiskie.nix
@@ -34,6 +38,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/2018c403-4ad2-4e41-9546-2193fff9db4d";
+  boot.kernelParams = [
+  	"iommu=pt"
+	"no_console_suspend"
+  ];
 
   networking.hostName = "Sakamoto"; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
